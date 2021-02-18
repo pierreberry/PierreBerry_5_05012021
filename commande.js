59ddd8b6c0e0e4be4ddb8e8f290acb17708fe75e
Pierre Berry <pierreberry@hotmail.fr>
2021-02-18 13:12:34 +0100

    style panier form

--- commande.js
+++ commande.js
@@ -1,3 +1,92 @@
+function getStorage() {
+
+    productStorage = [];
+    sum = 0;
+    JSON.parse(localStorage.getItem('cart')).forEach(data => {
+        let product = new Product(data.color, data.id, data.name, data.price, data.image, data.description);
+        productStorage.push(product);
+        sum += data.price;
+    })
+    createCart(productStorage);
+    additionPrice(sum);
+
+};
+
+function additionPrice(sum) {
+    const total = sum / 100 + ',' + sum % 100 + ' €'
+    finalPrice.innerHTML = `Prix total : ` + total;
+}
+
+function createCart(productStorage) {
+    //Get the container for each card
+    const panier = document.getElementById("panier");
+    //Loop for each teddies in the api
+    productStorage.forEach(product => {
+        let creation = displayCart(product);
+        panier.appendChild(creation);
+    });
+}
+
+
+function displayCart(product) {
+    //Creation of the bootstrap row
+    let row = document.createElement("div");
+    row.classList.add("row");
+    //Creation of the div.card
+    const cardCmd = document.createElement("div");
+    cardCmd.classList.add("cardCmd");
+    row.appendChild(cardCmd);
+    //Creation of the image element
+    const imageCmd = document.createElement("img");
+    imageCmd.src = product.image;
+    imageCmd.classList.add("cmd__image");
+    imageCmd.classList.add("shadow");
+    cardCmd.appendChild(imageCmd);
+    //Creation of the cmd__content
+    const contentCmd = document.createElement("div");
+    contentCmd.classList.add("cmd__content");
+    cardCmd.appendChild(contentCmd);
+    //Creation of the Name & Price
+    const cmdNP = document.createElement("div");
+    cmdNP.classList.add("cmd");
+    cmdNP.classList.add("cmd__name__prix");
+    contentCmd.appendChild(cmdNP);
+    //Creation of p name
+    const cmdName = document.createElement("p");
+    cmdName.innerHTML = product.name;
+    cmdNP.appendChild(cmdName);
+    //Creation of p price
+    const cmdPrice = document.createElement("p");
+    cmdPrice.innerHTML = product.getPrice();
+    cmdNP.appendChild(cmdPrice);
+    //Creation of the Color & Button
+    const cmdCB = document.createElement("div");
+    cmdCB.classList.add("cmd");
+    cmdCB.classList.add("cmd__color__btn");
+    contentCmd.appendChild(cmdCB);
+    //Creation of span name
+    const cmdColor = document.createElement("span");
+    cmdColor.classList.add("cmd__color");
+    cmdColor.style.backgroundColor = product.colors;
+    cmdCB.appendChild(cmdColor);
+    //Creation of button price
+    const cmdTrash = document.createElement("button");
+    cmdTrash.classList.add("btn");
+    cmdTrash.classList.add("trash");
+    cmdCB.appendChild(cmdTrash);
+    //Icon trash
+    const iconTrash = document.createElement("i");
+    iconTrash.classList.add("fas");
+    iconTrash.classList.add("fa-trash");
+    cmdTrash.appendChild(iconTrash);
+    //Creation of the p description
+    const cmdDescription = document.createElement("p");
+    cmdDescription.innerHTML = product.description;
+    contentCmd.appendChild(cmdDescription);
+    return row;
+
+}
+
 function showEmptyCart() {
     document.getElementById("panierVide").style.visibility = 'visible';
     document.getElementById("panierPlein").style.visibility = 'hidden';
@@ -19,110 +108,11 @@ document.getElementById("clearStorage").addEventListener('click', (e) => {
     showEmptyCart();
 })
 
-function getStorage() {
+const listBtn = document.querySelectorAll(`div .cardCmd`)
 
-    productStorage = [];
-    totalPrice = [];
-    JSON.parse(localStorage.getItem('cart')).forEach(data => {
-        let product = new Product(data.color, data.id, data.name, data.price, data.image, data.description);
-        productStorage.push(product);
-        totalPrice.push(data.price);
-    })
-    createCart(productStorage);
-    additionPrice(totalPrice);
-
-};
-
-function additionPrice(totalPrice) {
-    sum = 0;
-    totalPrice.forEach(dataPrice => {
-        sum += dataPrice;
-    })
-    const total = sum / 100 + ',' + sum % 100 + ' €'
-    console.log(total);
-}
-
-function createCart(productStorage) {
-    //Get the container for each card
-    const panier = document.getElementById("panier");
-    //Loop for each teddies in the api
-    productStorage.forEach(product => {
-        let creation = displayCart(product);
-        panier.appendChild(creation);
-    });
-}
+console.log(listBtn);
 
 
-function displayCart(product) {
-    //Creation of the bootstrap col
-    const col = document.createElement("div");
-    col.classList.add("col");
-    //Creation of the div.card
-    const newCard = document.createElement("div");
-    newCard.classList.add("card");
-    col.appendChild(newCard);
-    //Creation of the link on the image a.lien__image
-    const cardLink = document.createElement("a");
-    cardLink.classList.add("lien__image");
-    newCard.appendChild(cardLink);
-    cardLink.href = `produits.html?id=` +
-        product.id;
-    //Creation of the img element img.card-img-top.shadow-sm
-    const newImg = document.createElement("img");
-    newImg.classList.add("card-img-top");
-    newImg.classList.add("shadow-sm");
-    cardLink.appendChild(newImg);
-    newImg.src = product.image;
-    //Creation of the the div.card-body
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
-    newCard.appendChild(cardBody);
-    //Creation of the teddies price element h5.card-title
-    let storePrice = document.createElement("input");
-    storePrice.classList.add("number");
-    storePrice.setAttribute("disabled", true);
-    storePrice.setAttribute("value", product.getPrice());
-    cardBody.appendChild(storePrice);
-
-
-    //Creation of the teddies name element p.card-text with link
-    const newName = document.createElement("p");
-    newName.classList.add("card-text");
-    cardBody.appendChild(newName);
-    // Name link on the products
-    newName.innerHTML =
-        `<a href='produits.html?id=` +
-        product.id +
-        `'>` +
-        product.name +
-        `</a>`;
-
-    const newColor = document.createElement("span");
-    newColor.classList.add("teddies__color");
-    newColor.style.backgroundColor = product.colors;
-    cardBody.appendChild(newColor);
-
-    return col;
-    /*     const displayStore = document.createElement("div");
-        displayStore.classList.add("cart__container");
-        panier.appendChild(displayStore);
-        const newH2 = document.createElement("h2");
-        newH2.classList.add("teddies__name");
-        newH2.innerHTML = product.name;
-        displayStore.appendChild(newH2);
-        const newImage = document.createElement("img");
-        newImage.classList.add("teddies__picture");
-        newImage.src = product.image;
-        displayStore.appendChild(newImage);
-        const newColor = document.createElement("span");
-        newColor.classList.add("teddies__color");
-        newColor.style.backgroundColor = product.colors;
-        displayStore.appendChild(newColor);
-        let storePrice = document.createElement("input");
-        storePrice.classList.add("number");
-        storePrice.setAttribute("disabled", true);
-        storePrice.setAttribute("value", product.price / 100 + ',' + product.price % 100 + ' €')
-        displayStore.appendChild(storePrice); */
-}
 
 getStorage();
+
